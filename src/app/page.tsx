78efe79db8395923eb7a1b9fe044610d58e94e0e
Ben Chigozie <115552805+benchigozie@users.noreply.github.com.>
2026-02-03 19:08:26 +0100

    feat: add cta section and render in homepage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import AlphaCircle from "@/components/AlphaCircle";
+import CTA from "@/components/CTA";
 import EliteSpaces from "@/components/EliteSpaces";
 import Hero from "@/components/Hero";
 import Offer from "@/components/Offer";
@@ -12,6 +13,7 @@ export default function Home() {
       <EliteSpaces/>
       <Protocol />
       <AlphaCircle />
+      <CTA />
    </div>
   );
 }
