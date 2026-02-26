142761c02a6faa1e02a75742d1705a85add17d7f
Ben Chigozie <115552805+benchigozie@users.noreply.github.com.>
2026-02-27 00:32:53 +0100

    feat: add first meta pixel.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,6 +50,23 @@ export default function RootLayout({
             gtag('config', '${GA_ID}');
           `}
         </Script>
+        <Script>{`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+            n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+          '[https://connect.facebook.net/en_US/fbevents.js](https://connect.facebook.net/en_US/fbevents.js)');
+          fbq('init', '1637731120440004');
+          fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript><img height="1" width="1" style={{ display: "none" }}
+          src="[https://www.facebook.com/tr?id=1637731120440004&ev=PageView&noscript=1](https://www.facebook.com/tr?id=1637731120440004&ev=PageView&noscript=1)"
+        /></noscript>
+
       </head>
       <body
         className={`${geistSans.variable} ${inter.variable} antialiased`}
